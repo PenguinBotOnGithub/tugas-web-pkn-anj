de0196f8d6ff3e5bb8e4f950ab525ecc386269ce
PenguinBotOnGithub <penguinbotonline@protonmail.com>
2023-11-23 09:53:11 +0700

    Styling banner

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,13 @@
 import NavigationBar from "../../components/navigationbar/NavigationBar";
+import "./Home.css";
 
 function Home() {
     return (
         <>
             <NavigationBar />
+            <div class="banner">
+                <h1>Budaya Kudus</h1>
+            </div>
         </>
     );
 
